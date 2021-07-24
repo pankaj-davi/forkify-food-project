53c8387d20ff27039355c8d93146796e20404477
kapildavi <menariapankaj17@gmail.com>
2021-07-24 15:56:50 +0530

    add new Welcome Featur

--- src/js/controller.js
+++ src/js/controller.js
@@ -137,6 +137,10 @@ const controlAddRecipe = async newRecipe => {
   }
 };
 
+const newFeature = () => {
+  console.log('welcome');
+};
+
 const init = function () {
   bookMarkView.addHandlerRender(constrolBookmarks);
   recipeView.addHenderRender(controlRecipes);
@@ -145,5 +149,6 @@ const init = function () {
   searchView.addHendlerSearch(controlSearchResults);
   paginationView.addHendlerClick(controalPagenation);
   addRecipeView.addEventUpload(controlAddRecipe);
+  newFeature();
 };
 init();
